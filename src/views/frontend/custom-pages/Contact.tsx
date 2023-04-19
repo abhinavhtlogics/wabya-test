ac5ad67c02ec02e79a7098acd1428c4339774edf
ishita <ishita@htlogics.com>
2023-04-19 14:51:15 +0530

    changes_19April_2023

--- src/views/frontend/custom-pages/Contact.tsx
+++ src/views/frontend/custom-pages/Contact.tsx
@@ -1,39 +1,73 @@
 import Link from "next/link"
-import { app, database } from '../../../../firebaseConfig'
+import { database } from '../../../../firebaseConfig'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
-import { getAuth } from 'firebase/auth'
-import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { collection, addDoc } from 'firebase/firestore';
+
+// form validation
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+import { Alert } from '@mui/material'
 
 const Contact = () => {
 
-  const [ID, setID] = useState(null);
-  const [contactName, setName] = useState('');
-  const [contactEmail, setEmail] = useState('');
-  const [contactMessage, setMessage] = useState('');
-  const [fireData, setFireData] = useState([]);
-  const [isUpdate, setIsUpdate] = useState(false);
-  const databaseRef = collection(database, 'Contact');
-  let router = useRouter()
+  const router = useRouter()
+  const [message, setErrorMsg] = useState(false);
 
-  const addData = () => {
-    addDoc(databaseRef, {
-      contactName: contactName,
-      contactEmail: contactEmail,
-      contactMessage: contactMessage,
-    })
-      .then(() => {
-        alert('Contact details added successfully.')
-
-        // getData()
-        setName('')
-        setEmail('')
-        setMessage('')
-      })
-      .catch((err) => {
-        console.error(err);
-      })
+  const initialValues = {
+    contactName: "",
+    contactEmail: "",
+    contactMessage : "",
+    contactChoose : "",
   }
+
+  //  yup form validation
+  const signUpSchema = Yup.object({
+    contactName: Yup.string().min(2).max(25).required("Name field is required"),
+    contactEmail: Yup.string().email().required("Email field is required"),
+    contactMessage: Yup.string().required("Message field is required"),
+    // contactChoose: Yup.string().required("Please choose any option"),
+  });
+
+  const contactRef = collection(database, 'Contact');
+
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+
+    initialValues : initialValues,
+    validationSchema : signUpSchema,
+    validateOnMount: false,
+    validateOnBlur: false,
+    validateOnChange: false,
+
+    onSubmit: (values, action) => {
+      console.log(
+        "🚀 ~ file: index.tsx ~ line 81 ~ Registration ~ values",
+        values,
+
+          addDoc(contactRef, {
+            contactName: values.contactName,
+            contactEmail : values.contactEmail,
+            contactMessage : values.contactMessage,
+            contactChoose : values.contactChoose,
+          })
+
+            .then(() => {
+              setErrorMsg(true);
+            })
+            .catch((err) => {
+              console.error(err);
+            })
+
+      );
+      action.resetForm();
+    },
+  });
+
+    console.log(
+      "🚀 ~ file: index.tsx ~ line 90 ~ Registration ~ errors",
+      errors
+    );
 
 
   return(
@@ -53,18 +87,49 @@ const Contact = () => {
 
           <div className="col-sm-6 right">
             <div className="inner">
-            <form noValidate autoComplete='off' onSubmit={e => e.preventDefault()}>
-              <div className="form-group"><input className="form-control" placeholder="Name" type="text" name="contactName" onChange={event => setName(event.target.value)} value={ contactName } /></div>
-              <div className="form-group"><input className="form-control" placeholder="Email" type="email" name="contactEmail" onChange={event => setEmail(event.target.value)} value={ contactEmail } /></div>
+            {message ? (
+              <Alert  className="text-left mb-4"> Thank you for showing interest. Our Wabya team will be contact you shortly.</Alert>
+              ) : null}
+
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+              <div className="form-group"><input className="form-control" placeholder="Name" type="text" name="contactName" value={values.contactName} onChange={handleChange} onBlur={handleBlur} />
+              </div>
+              {
+              errors.contactName && touched.contactName ?
+                (
+                  <p className="form-error">*{errors.contactName}</p>
+                ) : null
+              }
+              <div className="form-group"><input className="form-control" placeholder="Email" type="email" name="contactEmail" value={values.contactEmail} onChange={handleChange} onBlur={handleBlur} />
+              </div>
+              {
+              errors.contactEmail && touched.contactEmail ?
+                (
+                  <p className="form-error">*{errors.contactEmail}</p>
+                ) : null
+              }
               <div className="form-group left">
                 <small><strong>I’m a</strong></small>
 
-                <span><input type="radio" name="contactChoose" id="choose_type" /> coach</span>
-                <span><input type="radio" name="contactChoose" id="choose_type" /> client</span>
-                <span><input type="radio" name="contactChoose" id="choose_type" /> curious visitor</span>
+                <span><input type="radio" name="contactChoose" id="choose_type" value='coach' /> coach</span>
+                <span><input type="radio" name="contactChoose" id="choose_type" value='client' /> client</span>
+                <span><input type="radio" name="contactChoose" id="choose_type" value='curious_visitor' /> curious visitor</span>
+                </div>
+                {
+                  errors.contactChoose && touched.contactChoose ?
+                  (
+                    <p className="form-error">*{errors.contactChoose}</p>
+                  ) : null
+                }
+              <div className="form-group"><textarea className="form-control" placeholder="Message" name="contactMessage" value={values.contactMessage} onChange={handleChange} onBlur={handleBlur}></textarea>
               </div>
-              <div className="form-group"><textarea className="form-control" placeholder="Message" name="contactMessage" onChange={event => setMessage(event.target.value)}>{contactMessage}</textarea></div>
-            <div className="form-group" style={{ textAlign: 'left'}}><button className="btn" type="submit" onClick={addData}>send</button></div>
+              {
+                errors.contactMessage && touched.contactMessage ?
+                (
+                  <p className="form-error">*{errors.contactMessage}</p>
+                ) : null
+              }
+            <div className="form-group" style={{ textAlign: 'left'}}><button className="btn" type="submit" >send</button></div>
             </form>
             </div>
           </div> {/* <!--/ col-sm --> */}
